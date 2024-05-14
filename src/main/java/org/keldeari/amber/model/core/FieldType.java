77979476bbca83b2302dd1f901079dd57b4f49ec
Maxim Kan <max-kan@outlook.com>
2024-05-14 12:07:32 +0900

    style: fix IDEA warnings

--- src/main/java/org/keldeari/amber/model/core/FieldType.java
+++ src/main/java/org/keldeari/amber/model/core/FieldType.java
@@ -1,5 +1,5 @@
 package org.keldeari.amber.model.core;
 
 public enum FieldType {
-    INTEGER, STRING, BOOLEAN, DOUBLE, DATETIMEUTC, NODE;
+    INTEGER, STRING, BOOLEAN, DOUBLE, DATETIMEUTC, NODE
 }
